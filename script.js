be44230d4df36c9fa32faba47b3707fb148a3aa0
Tobias  Larsen <tobiaslarsen@MacBook-Pro-tilhrende-Tobias.local>
2020-09-11 13:13:21 +0200

    initial remove valgt

--- script.js
+++ script.js
@@ -1,93 +1,11 @@
-/*let retter;
-let albums;
-let filter = "alle"
-const popop = document.querySelector("#popop");
-document.addEventListener("DOMContentLoaded", loadJSON)
-
-
-async function loadJSON() {
-    const loadJSON = await fetch("https://spreadsheets.google.com/feeds/list/1BGsWxPo26S0q3MziijrBCP8GVN1qP2hOO3wCT_hygCk/od6/public/values?alt=json")
-    albums = await loadJSON.json();
-    addEventListenerToButtons();
-    visAlbums();
-}
-
-function visAlbums() {
-    const templatePointer = document.querySelector("template");
-    const listPointer = document.querySelector(".loopview");
-    listPointer.innerHTML = "";
-    albums.feed.entry.forEach(ret => {
-        if (filter == "alle" || filter == ret.gsx$genre.$t.toLowerCase()) {
-
-            const klon = templatePointer.cloneNode(true).content;
-            klon.querySelector(".navn").textContent = ret.gsx$navn.$t;
-            klon.querySelector(".genre").textContent = ret.gsx$genre.$t;
-            klon.querySelector(".kunstner").textContent = ret.gsx$kunstner.$t;
-
-
-
-            klon.querySelector("img").src = ret.gsx$billede.$t;
-
-
-
-
-            klon.querySelector("article").addEventListener("click", () => visDetaljer(ret))
-
-            listPointer.appendChild(klon);
-        }
-    })
-}
-
-
-function visDetaljer(ret) {
-    popop.style.display = "block";
-    popop.querySelector("h2").textContent = ret.gsx$navn.$t;
-    popop.querySelector(".lang").textContent = ret.gsx$lang.$t;
-    popop.querySelector(".oprindelse").textContent = ret.gsx$oprindelse.$t;
-    popop.querySelector(".pris").textContent = ret.gsx$pris.$t + "kr";
-
-    popop.querySelector("img").src = "imgs/large/" + ret.gsx$billede.$t + ".jpg";
-
-
-
-
-
-}
-
-document.querySelector("#luk").addEventListener("click", () => popop.style.display = "none");
-
-
-
-
-
-function addEventListenerToButtons() {
-    document.querySelectorAll(".filter").forEach((btn) => {
-        btn.addEventListener("click", filterBTNs);
-    });
-}
-
-function filterBTNs() {
-    filter = this.dataset.genre;
-    console.log(filter)
-    document.querySelector("h2").textContent = this.textContent;
-    document.querySelectorAll(".filter").forEach((btn) => {
-        btn.classList.remove("valgt");
-
-    })
-    this.classList.add("valgt");
-    visAlbums();
-
-
-
-}*/
-
-
 let retter;
 let albums;
 let filter = "alle";
 let filterpop = "";
 let filterrock = "";
 let filterjazz = "";
+let filterhiphop = "";
+let filterEdm = "";
 const popop = document.querySelector("#popop");
 
 document.addEventListener("DOMContentLoaded", loadJSON)
@@ -106,7 +24,7 @@ function visAlbums() {
     const listPointer = document.querySelector(".loopview");
     listPointer.innerHTML = "";
     albums.feed.entry.forEach(album => {
-        if (filter == "alle" || filterpop == album.gsx$genre.$t.toLowerCase() || filterrock == album.gsx$genre.$t.toLowerCase() || filterjazz == album.gsx$genre.$t.toLowerCase()) {
+        if (filter == "alle" || filterpop == album.gsx$genre.$t.toLowerCase() || filterrock == album.gsx$genre.$t.toLowerCase() || filterjazz == album.gsx$genre.$t.toLowerCase() || filterhiphop == album.gsx$genre.$t.toLowerCase() || filterEdm == album.gsx$genre.$t.toLowerCase()) {
             console.log(album);
             const klon = templatePointer.cloneNode(true).content;
             klon.querySelector(".navn").textContent = album.gsx$navn.$t;
@@ -145,8 +63,10 @@ document.querySelector("#luk").addEventListener("click", () => popop.style.displ
 
 function addEventListenersToButtons() {
     document.querySelectorAll(".filter").forEach((btn) => {
-
         btn.addEventListener("click", filterBTNs);
+
+
+        console.log(filter)
 
     });
 
@@ -155,8 +75,13 @@ function addEventListenersToButtons() {
 
 function filterBTNs() {
     filter = this.dataset.genre;
+
     this.classList.toggle("valgt");
+
+    document.querySelector("#filter-alle").classList.remove("valgt");
     console.log(filter)
+
+
     if (this.dataset.genre == "pop") {
         if (filterpop == "") {
 
@@ -179,7 +104,24 @@ function filterBTNs() {
         } else {
             filterjazz = "";
         }
+    } else if (this.dataset.genre == "edm") {
+        if (filterEdm == "") {
+            filterEdm = "edm";
+
+        } else {
+            filterEdm = "";
+        }
+    } else if (this.dataset.genre == "hip-hop") {
+        if (filterhiphop == "") {
+            filterhiphop = "hip-hop";
+
+        } else {
+            filterhiphop = "";
+        }
+
     }
+
+
 
     visAlbums();
 
