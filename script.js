e9d5235c9ad0f38d5cbb56386f098c20914b8168
Tobias  Larsen <tobiaslarsen@MacBook-Pro-tilhrende-Tobias.local>
2020-09-16 09:47:02 +0200

    multi filter færdig uden alle funktion

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let albums;
+let filterår = "alle";
 let filter = "alle";
 let filterpop = "";
 let filterrock = "";
 let filterjazz = "";
 let filterhiphop = "";
 let filterEdm = "";
+let filter1980 = "";
+let filter1990 = "";
+let filter2000 = "";
+let filter2010 = "";
+let filter2020 = "";
 const popop = document.querySelector("#popop");
 const kategoriOpen = document.querySelector(".kategori");
 
@@ -14,6 +20,7 @@ async function loadJSON() {
     const JSONData = await fetch("https://spreadsheets.google.com/feeds/list/1BGsWxPo26S0q3MziijrBCP8GVN1qP2hOO3wCT_hygCk/od6/public/values?alt=json");
     albums = await JSONData.json();
     addEventListenersToButtons();
+    addEventListenersToButtons2();
     visAlbums();
 
 }
@@ -24,7 +31,7 @@ function visAlbums() {
     const listPointer = document.querySelector(".loopview");
     listPointer.innerHTML = "";
     albums.feed.entry.forEach(album => {
-        if (filter == "alle" || filterpop == album.gsx$genre.$t.toLowerCase() || filterrock == album.gsx$genre.$t.toLowerCase() || filterjazz == album.gsx$genre.$t.toLowerCase() || filterhiphop == album.gsx$genre.$t.toLowerCase() || filterEdm == album.gsx$genre.$t.toLowerCase()) {
+        if ((filter == "alle" || filterpop == album.gsx$genre.$t.toLowerCase() || filterrock == album.gsx$genre.$t.toLowerCase() || filterjazz == album.gsx$genre.$t.toLowerCase() || filterhiphop == album.gsx$genre.$t.toLowerCase() || filterEdm == album.gsx$genre.$t.toLowerCase()) && (filterår == "alle" || filter1980 == album.gsx$årstal.$t.toLowerCase() || filter1990 == album.gsx$årstal.$t.toLowerCase() || filter2000 == album.gsx$årstal.$t.toLowerCase() || filter2010 == album.gsx$årstal.$t.toLowerCase() || filter2020 == album.gsx$årstal.$t.toLowerCase())) {
             console.log(album);
             const klon = templatePointer.cloneNode(true).content;
             klon.querySelector(".navn").textContent = album.gsx$navn.$t;
@@ -73,19 +80,31 @@ function addEventListenersToButtons() {
 
 }
 
+function addEventListenersToButtons2() {
+    document.querySelectorAll(".filterår").forEach((btn) => {
+        btn.addEventListener("click", filterBTNs2);
+
+
+        console.log(filterår)
+
+    });
+
+
+}
+
 function filterBTNs() {
     filter = this.dataset.genre;
 
 
     this.classList.toggle("valgt");
 
-    document.querySelector("#filter-alle").classList.remove("valgt");
+
     console.log(filter)
 
 
     if (this.dataset.genre == "pop") {
         if (filterpop == "") {
-
+            document.querySelector("#filter-alle").classList.remove("valgt");
             filterpop = "pop";
 
         } else {
@@ -93,6 +112,7 @@ function filterBTNs() {
         }
     } else if (this.dataset.genre == "rock") {
         if (filterrock == "") {
+            document.querySelector("#filter-alle").classList.remove("valgt");
             filterrock = "rock";
 
         } else {
@@ -100,6 +120,7 @@ function filterBTNs() {
         }
     } else if (this.dataset.genre == "jazz") {
         if (filterjazz == "") {
+            document.querySelector("#filter-alle").classList.remove("valgt");
             filterjazz = "jazz";
 
         } else {
@@ -107,6 +128,7 @@ function filterBTNs() {
         }
     } else if (this.dataset.genre == "edm") {
         if (filterEdm == "") {
+            document.querySelector("#filter-alle").classList.remove("valgt");
             filterEdm = "edm";
 
         } else {
@@ -114,6 +136,7 @@ function filterBTNs() {
         }
     } else if (this.dataset.genre == "hip-hop") {
         if (filterhiphop == "") {
+            document.querySelector("#filter-alle").classList.remove("valgt");
             filterhiphop = "hip-hop";
 
         } else {
@@ -121,6 +144,59 @@ function filterBTNs() {
         }
 
     }
+
+    visAlbums();
+
+
+}
+
+
+function filterBTNs2() {
+    filterår = this.dataset.årstal;
+    console.log("filterBTNs2")
+
+
+    this.classList.toggle("valgt");
+
+
+
+
+    // Her starter årstal-filter
+
+    if (this.dataset.årstal == "1980") {
+        if (filter1980 == "") {
+
+            filter1980 = "1980";
+
+        } else {
+            filter1980 = "";
+        }
+    } else if (this.dataset.årstal == "1990") {
+        if (filter1990 == "") {
+            filter1990 = "1990";
+        } else {
+            filter1990 = "";
+        }
+    } else if (this.dataset.årstal == "2000") {
+        if (filter2000 == "") {
+            filter2000 = "2000";
+        } else {
+            filter2000 = "";
+        }
+    } else if (this.dataset.årstal == "2010") {
+        if (filter2010 == "") {
+            filter2010 = "2010";
+        } else {
+            filter2010 = "";
+        }
+    } else if (this.dataset.årstal == "2020") {
+        if (filter2020 == "") {
+            filter2020 = "2020";
+        } else {
+            filter2020 = "";
+        }
+    }
+
 
 
 
@@ -131,6 +207,11 @@ function filterBTNs() {
 
 
 
+
+
+
+
+
 document.querySelector(".pil_menu_open").addEventListener("click", openKategori);
 
 
@@ -138,7 +219,9 @@ document.querySelector(".pil_menu_open").addEventListener("click", openKategori)
 function openKategori() {
     console.log("openKategori");
 
-
+    document.querySelector("#mellem1").classList.toggle("display_filter")
+    document.querySelector("#mellem2").classList.toggle("display_filter")
+    document.querySelector("#mellem3").classList.toggle("display_filter")
     document.querySelector("#kategori_alle").classList.toggle("display_filter")
     document.querySelector("#kategori_alle1").classList.toggle("display_filter")
     document.querySelector("#kategori_alle2").classList.toggle("display_filter")
